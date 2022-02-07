2dd6ce1791df8e7b6433769cce3606b8a1ce5c18
vrajendra <vrajendra.bhavsar@mindinventory.com>
2022-02-07 15:35:29 +0530

    - Added Car and Shoes images - Added code to display car and shoes

--- app/src/main/java/com/erdemtsynduev/rotate360degree/MainActivity.kt
+++ app/src/main/java/com/erdemtsynduev/rotate360degree/MainActivity.kt
@@ -14,7 +14,9 @@ import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
-    private val arrayListPictureAssets: ArrayList<String> = ArrayList()
+    private val bottlePictureAssets: ArrayList<String> = ArrayList()
+    private val carPictureAssets: ArrayList<String> = ArrayList()
+    private val shoesPictureAssets: ArrayList<String> = ArrayList()
     var playImage = true
     var isReverse = true
     var indexImage = 0
@@ -70,9 +72,9 @@ class MainActivity : AppCompatActivity() {
             indexImage--
             checkNumberIndex()
             runOnUiThread {
-                Glide.with(this).load(arrayListPictureAssets[indexImage])
-                    .placeholder(main_activity_photo_image.drawable)
-                    .into(main_activity_photo_image)
+                Glide.with(this).load(bottlePictureAssets[indexImage])
+                    .placeholder(ivShoes_360.drawable)
+                    .into(ivShoes_360)
             }
             delay(50)
         }
@@ -83,9 +85,9 @@ class MainActivity : AppCompatActivity() {
             indexImage++
             checkNumberIndex()
             runOnUiThread {
-                Glide.with(this).load(arrayListPictureAssets[indexImage])
-                    .placeholder(main_activity_photo_image.drawable)
-                    .into(main_activity_photo_image)
+                Glide.with(this).load(bottlePictureAssets[indexImage])
+                    .placeholder(ivShoes_360.drawable)
+                    .into(ivShoes_360)
             }
             delay(50)
         }
@@ -102,9 +104,9 @@ class MainActivity : AppCompatActivity() {
         while (playImage) {
             checkNumberIndex()
             runOnUiThread {
-                Glide.with(this).load(arrayListPictureAssets[indexImage])
-                    .placeholder(main_activity_photo_image.drawable)
-                    .into(main_activity_photo_image)
+                Glide.with(this).load(bottlePictureAssets[indexImage])
+                    .placeholder(ivShoes_360.drawable)
+                    .into(ivShoes_360)
             }
             delay(100)
             increaseIndex()
@@ -129,41 +131,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createListAssetsImage() {
-        arrayListPictureAssets.add("file:///android_asset/AVF_2696.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2697.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2698.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2699.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2700.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2701.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2702.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2703.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2704.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2705.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2706.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2707.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2708.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2709.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2710.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2711.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2712.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2713.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2714.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2715.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2716.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2717.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2718.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2719.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2720.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2721.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2722.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2723.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2724.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2725.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2726.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2727.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2728.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2729.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2730.jpg")
-        arrayListPictureAssets.add("file:///android_asset/AVF_2731.jpg")
+        for(i in 2696..2731){
+            bottlePictureAssets.add("file:///android_asset/bottle/AVF_${i}.jpg")
+        }
     }
 }
